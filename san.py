3da4ca4f8708851ad40660c07768b541e0ca8ec7
hevzya <143340552+hevzya@users.noreply.github.com>
2025-02-17 12:11:29 +0200

    Update san.py

--- san.py
+++ san.py
@@ -1,1 +1,7 @@
-
+echo "# bon-voyage" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/hevzya/bon-voyage.git
+git push -u origin main
